f9f8a1bcf8910a490d66d2a1066ca31e3bffad93
Lizandra Lígia <lizandraligia@hotmail.com>
2017-06-20 14:30:27 -0300

    Update RepositorioSessões.java

--- src/dados/RepositorioSessões.java
+++ src/dados/RepositorioSessões.java
@@ -1,24 +1,34 @@
 package src.br.ufrpe.LsCine.repositorios;
-import src.br.ufrpe.LsCine.beans.Sess„o;
+import src.br.ufrpe.LsCine.beans.Sess√£o;
 import java.util.ArrayList;
 import java.util.List;
 
-public class RepositorioSessıes {
+public class RepositorioSess√µes {
 	
 	
-	private ArrayList<Sess„o> sessao;
+	private ArrayList<Sess√£o> sessao;
 	private int lim;
 	
-	public RepositorioSessıes() {
-		this.sessao = new ArrayList<Sess„o>();
+	public RepositorioSess√µes() {
+		this.sessao = new ArrayList<Sess√£o>();
 		this.lim = 0;
 	}
 	
-	public void adicionar(Sess„o sessao){
+	//CRIEI METODOS GET 
+	public ArrayList<Sess√£o> getSessao() {
+		return sessao;
+	}
+
+	public int getLim() {
+		return lim;
+	}
+	
+	//MUDEI O RETORNO DE void PARA boolean
+	public boolean adicionar(Sess√£o sessao){
 		
-		System.out.println();
+		/*System.out.println();
 		if (sessao == null) {
-			System.out.println("Sess„o inv·lida");
+			System.out.println("Sess√£o inv√°lida");
 			return;
 		}
 		
@@ -41,49 +51,50 @@ public class RepositorioSessıes {
 				System.out.println("Sala lotada!");
 				return;
 			}
-		}
+		}*/
 		
 
 		if(this.lim < 5) {
 			this.sessao.add(sessao);
 			this.lim++;
-			System.out.println("Sess„o cadastrada com sucesso!");
-			return;
+			//System.out.println("Sess√£o cadastrada com sucesso!");
+			return true;
 		}else{
-			System.out.println("Sala lotada!");
-			return;
+			//System.out.println("Sala lotada!");
+			return false;
 		}
 	}
 	
-	
-	public void remover(Sess„o sessao){
+	//MUDEI O RETORNO DE void PARA boolean
+	public boolean remover(Sess√£o sessao){
 		int search = pesquisar(sessao);
 		if (search != -1) {
 			this.sessao.remove(sessao);
-			System.out.println("Sess„o excluÌda.");
-			return;
+			//System.out.println("Sess√£o exclu√≠da.");
+			return true;
 		}
-		System.out.println("Sess„o n„o encontrada.");
-		
+		//System.out.println("Sess√£o n√£o encontrada.");
+		return false;
 	}
 	
-	public void alterar(Sess„o sessao){
+	//MUDEI O RETORNO DE void PARA boolean
+	public boolean alterar(Sess√£o sessao){
 		int search = pesquisar(sessao);
 		if (search != -1) {
 			this.sessao.set(search, sessao);
-			System.out.println("Sess„o alterada.");
-			return;
+			//System.out.println("Sess√£o alterada.");
+			return true;
 		}
-		System.out.println("Sess„o n„o encontrada.");
-		
+		//System.out.println("Sess√£o n√£o encontrada.");
+		return false;
 	}
 	
-	public int pesquisar(Sess„o sessao){
+	public int pesquisar(Sess√£o sessao){
 		return this.sessao.indexOf(sessao);
 	}
 	
-	public List<Sess„o> listar() {
-		List<Sess„o> listaOrdenada = this.sessao;
+	public List<Sess√£o> listar() {
+		List<Sess√£o> listaOrdenada = this.sessao;
 		return listaOrdenada;
 	}
 
