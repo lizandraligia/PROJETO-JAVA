dbbb7f0c51ef0fb8ae6cff3fe9e250251a8a52e3
Luks753 <lucas753159@hotmail.com.br>
2017-06-25 16:10:36 -0300

    atualizado

--- src/br/ufrpe/LsCine/negocio/CadastroConta.java
+++ src/br/ufrpe/LsCine/negocio/CadastroConta.java
@@ -1,6 +1,8 @@
 package br.ufrpe.LsCine.negocio;
 import br.ufrpe.LsCine.negocio.beans.Conta;
+import java.util.ArrayList;
 import br.ufrpe.LsCine.dados.RepositorioConta;
+
 
 public class CadastroConta {
 
@@ -10,21 +12,19 @@ private RepositorioConta repositorioConta;
 		this.repositorioConta = new RepositorioConta();
 	}
 	
-	public boolean logar(String login, String senha){
-		Conta conta = new Conta(login, senha);
-		for(int i = 0; i < this.repositorioConta.getContas().size(); i++){
-			if(this.repositorioConta.getContas().get(i).equals(conta)){
-				return true;
-			}
-		}
-		return false;
-		
+	public boolean logar(Conta login){
+		return this.repositorioConta.logar(login);		
 	}
+	
 	public void adicionarConta(Conta conta){
 		this.repositorioConta.adicionar(conta);
 	}
 	
-	public void removerConta(Conta conta){
-		this.repositorioConta.remover(conta);
+	public void removerConta(String login){
+		this.repositorioConta.remover(login);
+	}
+	
+	public void listaContas(){
+		this.repositorioConta.listar();
 	}
 }
