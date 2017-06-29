06a05e3fdba1255ba07162648d59c5aefe65212e
Lizandra Lígia <lizandraligia@hotmail.com>
2017-06-28 21:18:06 -0300

    Update Conta.java

--- src/br/ufrpe/LsCine/negocio/beans/Conta.java
+++ src/br/ufrpe/LsCine/negocio/beans/Conta.java
@@ -17,17 +17,9 @@ public class Conta {
 	public String getLogin() {
 		return login;
 	}
-
-	public void setLogin(String login) {
-		this.login = login;
-	}
 		
 	public String getSenha() {
 		return senha;
-	}
-
-	public void setSenha(String senha) {
-		this.senha = senha;
 	}
 	
 	public boolean equals(Conta conta){
@@ -38,9 +30,4 @@ public class Conta {
 		}
 		return false;
 	}
-
-	public String toString() {
-		return "Conta [login=" + login + ", senha=" + senha + "]";
-	}
-
 }
