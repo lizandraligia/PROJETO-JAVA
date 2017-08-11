d5e03ed53839c5755fb62c44eb570f1d4c025b3f
Lizandra Lígia <lizandraligia@hotmail.com>
2017-08-11 09:46:06 -0300

    Update Main.java

--- src/br/ufrpe/LsCine/gui/Main.java
+++ src/br/ufrpe/LsCine/gui/Main.java
@@ -29,7 +29,6 @@ public class Main extends Application {
 	
 	public static void main(String[] args) {
 		launch(args);
-		Fachada sistema = Fachada.getInstancia();
 	}
 	
 	public static BorderPane getRoot() { 
