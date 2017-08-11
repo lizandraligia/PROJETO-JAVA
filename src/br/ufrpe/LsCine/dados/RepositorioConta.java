388dc191e07079234e22230d84cba34c91af9675
Lizandra Lígia <lizandraligia@hotmail.com>
2017-08-11 09:56:44 -0300

    Update RepositorioConta.java

--- src/br/ufrpe/LsCine/dados/RepositorioConta.java
+++ src/br/ufrpe/LsCine/dados/RepositorioConta.java
@@ -1,108 +1,57 @@
 package br.ufrpe.LsCine.dados;
+
 import br.ufrpe.LsCine.negocio.beans.Conta;
+import java.util.ArrayList;
+import br.ufrpe.LsCine.interfaces.IRepositorioConta;
 
-public class RepositorioConta {
+public class RepositorioConta implements IRepositorioConta{
 
-	private Conta[] contas;
-	private int lim;
+	private ArrayList<Conta> contas;
 	
 	public RepositorioConta(){
 		Conta user = new Conta("admin","admin");
-		this.contas = new Conta[10];
-		this.contas[0] = user;
-		this.lim = 1;
+		this.contas = new ArrayList<Conta>();
+		this.contas.add(user);
 	}
-	
-	
-	public Conta[] getContas() {
+
+	public ArrayList<Conta> getContas() {
 		return contas;
 	}
 
+	public void setContas(ArrayList<Conta> contas) {
+		this.contas = contas;
+	}
 
 	public boolean adicionar(Conta conta){
-		this.contas[this.lim]=conta;
-		this.lim++;
+		this.contas.add(conta);
 		return true;
 
 	}
 	
-	public boolean existe(String login) {
-		return this.retornarPosicaoLogin(login)==-1;
-	}
-	
-	public Conta buscarLogin(String nome) {		
-		int posicao = this.retornarPosicaoLogin(nome);		
-		return (posicao != -1) ? this.contas[posicao] : null;
-	}
-	
-	private int retornarPosicaoLogin(String nome) {		
-		for (int i = 0; i< this.lim; i++){
-			String cod = contas[i].getLogin();
-			if (cod.equals(nome)) {
-				return i;
-			} 
+	public Conta buscar(Conta conta){
+		if(conta == null){
+			return null;
 		}
-		
-		return -1;
-	}
-	
-	public Conta buscarSenha(String nome) {		
-		int posicao = this.retornarPosicaoSenha(nome);		
-		return (posicao != -1) ? this.contas[posicao] : null;
-	}
-	
-	private int retornarPosicaoSenha(String nome) {		
-		for (int i = 0; i< this.lim; i++){
-			String cod = contas[i].getSenha();
-			if (cod.equals(nome)) {
-				return i;
-			} 
-		}
-		
-		return -1;
-	}
-	
-	public boolean remover(String login) {
-		int ContaRemover = this.retornarPosicaoLogin(login);
-		
-		if (ContaRemover == -1) {
-			return false;
-		}
-		
-		this.contas[ContaRemover] = this.contas[this.lim -1];
-		this.contas[this.lim - 1] = null;
-		this.lim = this.lim - 1;
-		return true;
-		
-	}
-	
-	public boolean alterar(Conta conta) {		
-		int contaalterar = this.retornarPosicaoLogin(conta.getLogin());
-		if (contaalterar == -1) {
-			return false;
-		}
-		this.contas[contaalterar] = conta;
-		return true;
-	}
-	
-	public void listar() {
-		for(int i = 0; i<lim; i++){
-			System.out.println(contas[i].toString());
-			System.out.println();			
-		}
-	}
-	
-	public boolean logar(Conta login){
-		String log = login.getLogin();
-		String sen = login.getSenha();
-		for(int i=0; i<10; i++){
-			if(this.buscarLogin(log)!= null){
-				if(this.buscarSenha(sen)!= null){
-					return true;
+		for (int i = 0; i<contas.size() ; i++){	
+			if(conta.equals(contas.get(i).getLogin())){
+				if(conta.equals(contas.get(i).getSenha())){
+				return contas.get(i);
 				}
-			}
+			}			
 		}
-		return false;
+		return null;
 	}
 	
+	
+	public boolean remover(Conta login) {
+		if(login==null){
+			return false;
+		}
+		Conta posicao = this.buscar(login);
+		if(posicao != null){
+			this.contas.remove(posicao);
+			return true;
+		}
+		return false;	
+	}
 }
