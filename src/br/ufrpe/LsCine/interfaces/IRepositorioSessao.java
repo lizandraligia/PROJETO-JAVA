ea1795b80a45949089bb52d9a5542cfb6a3dcbc6
Luks753 <lucas753159@hotmail.com.br>
2017-08-12 15:14:37 -0300

    atualizado

--- src/br/ufrpe/LsCine/interfaces/IRepositorioSessao.java
+++ src/br/ufrpe/LsCine/interfaces/IRepositorioSessao.java
@@ -1,5 +1,15 @@
 package br.ufrpe.LsCine.interfaces;
 
+import java.util.ArrayList;
+import br.ufrpe.LsCine.negocio.beans.Sessao;
+
 public interface IRepositorioSessao {
 
+	public boolean adicionar(Sessao sessao);	
+	public Sessao procurar(int codigo);	
+	public boolean remover(int numero);	
+	public boolean alterar(Sessao sala);	
+	public ArrayList<Sessao> listar();
+	public int buscar(Sessao sala);	
+	
 }
