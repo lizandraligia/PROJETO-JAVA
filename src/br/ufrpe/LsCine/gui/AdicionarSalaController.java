dbc0480d4f5e73863acc8c45bc2d2b02d8c78a69
Luks753 <lucas753159@hotmail.com.br>
2017-08-13 12:21:40 -0300

    Adicioanr Sala

---
+++ src/br/ufrpe/LsCine/gui/AdicionarSalaController.java
@@ -0,0 +1,55 @@
+package br.ufrpe.LsCine.gui;
+
+import com.jfoenix.controls.JFXTextField;
+import com.jfoenix.controls.JFXCheckBox;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import br.ufrpe.LsCine.negocio.Fachada;
+import br.ufrpe.LsCine.negocio.beans.Salas;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+
+public class AdicionarSalaController implements Initializable {
+	
+	private Fachada fachada = Fachada.getInstancia();
+	
+	@FXML JFXTextField JXID;
+	@FXML JFXCheckBox s3D = new JFXCheckBox();
+	@FXML JFXCheckBox IMAX = new JFXCheckBox();
+	
+	
+	
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		
+	}
+	
+	public void inserir(){
+		try{
+			Salas sala = new Salas(Integer.parseInt(JXID.getText()), s3D.selectedProperty().get(), IMAX.selectedProperty().get());
+			fachada.getInstancia().getCadastroSa().adicionar(sala);
+			for(int i=0; i<fachada.getInstancia().getCadastroSa().listar().size(); i++){
+				System.out.println(fachada.getInstancia().getCadastroSa().listar().get(i).toString());
+			}
+			
+			
+		}
+		catch(Exception e){
+			System.out.println(e.getMessage());
+		}
+	}
+
+	public void voltar(){
+		try{
+			Telas.getInstance().getMenu();
+			System.out.println("funfou");
+		}
+		catch(Exception e){
+			System.out.println(e.getMessage());
+		}
+		
+	}
+
+}
