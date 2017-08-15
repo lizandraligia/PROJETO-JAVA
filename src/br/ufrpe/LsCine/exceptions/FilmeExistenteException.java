c62f7103ecf72cc35b733c37210b04eacd95a72b
Lizandra Lígia <lizandraligia@hotmail.com>
2017-08-15 06:10:58 -0300

    Update FilmeExistenteException.java

--- src/br/ufrpe/LsCine/exceptions/FilmeExistenteException.java
+++ src/br/ufrpe/LsCine/exceptions/FilmeExistenteException.java
@@ -3,8 +3,12 @@ package br.ufrpe.LsCine.exceptions;
 public class FilmeExistenteException extends Exception {
 	
 	public FilmeExistenteException() {
-		
-		super("Filme ja cadastrado no sistema.");
+		Alert alert = new Alert(AlertType.INFORMATION);
+		alert.setTitle("LsCine");
+		alert.setHeaderText(null);
+		alert.setContentText("Filme ja cadastrado no sistema.");
+		alert.showAndWait();
+		//super("Filme ja cadastrado no sistema.");
 	}
 	
 }
