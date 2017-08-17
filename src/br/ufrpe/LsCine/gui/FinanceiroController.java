7dfdd84e7c8da5a2013edbb634fbecd87499ebbd
Lizandra Lígia <lizandraligia@hotmail.com>
2017-08-16 23:59:05 -0300

    Update FinanceiroController.java

--- src/br/ufrpe/LsCine/gui/FinanceiroController.java
+++ src/br/ufrpe/LsCine/gui/FinanceiroController.java
@@ -1,5 +1,15 @@
 package br.ufrpe.LsCine.gui;
 
 public class FinanceiroController {
+  
+  public void voltar(){
+		try{
+			Telas.getInstance().getMenu();
+			System.out.println("funfou");
+		}
+		catch(Exception e){
+			System.out.println(e.getMessage());
+		}
+	}
 
 }
