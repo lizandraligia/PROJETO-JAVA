87953757087134819ba1c077f484485608d0bea9
Lizandra Lígia <lizandraligia@hotmail.com>
2017-08-23 09:30:48 -0300

    Update IDSalaInvalidoException.java

--- src/br/ufrpe/LsCine/exceptions/IDSalaInvalidoException.java
+++ src/br/ufrpe/LsCine/exceptions/IDSalaInvalidoException.java
@@ -2,6 +2,9 @@ package br.ufrpe.LsCine.exceptions;
 
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
 
 public class IDSalaInvalidoException extends Exception {
 	
